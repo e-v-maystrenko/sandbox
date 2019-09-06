23b3fc3d89e537e68114351961e4b2c4439da4f2
e.v.maystrenko <e.v.maystrenko@ukr.net>
2019-09-06 10:46:28 +0300

    Message2ds

---
+++ src/main/java/subdir/AbstractTest.java
@@ -0,0 +1,32 @@
+package subdir;
+
+public class AbstractTest {
+    public static void main(String[] args) {
+        AbstractEntity realEntity = new RealEntity();
+        realEntity.doSomething(); // RealEntity::doSomething
+
+        AbstractEntity unknownEntity = new NullEntity();
+        unknownEntity.doSomething(); // no output
+    }
+}
+
+abstract class AbstractEntity {
+
+    public abstract void doSomething();
+}
+
+class RealEntity extends AbstractEntity {
+
+    @Override
+    public void doSomething() {
+        System.out.println("RealEntity::doSomething");
+    }
+}
+
+class NullEntity extends AbstractEntity {
+
+    @Override
+    public void doSomething() {
+
+    }
+}
